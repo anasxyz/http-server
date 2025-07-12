2afb42597a84c84b23d8a8613e0d39a7dca11720
anasxyz <anaswessam176@gmail.com>
2025-07-12 14:29:52 +0100

    created test server with basic HTTP 200 response

--- server.c
+++ server.c
@@ -6,11 +6,10 @@ struct Server server_constructor(
     int domain,       // address domain, AF_INET for ipv4
     int service,      // type of service, SOCK_STREAM for tcp
     int protocol,     // 0 means default protocol for given domain and service
-    u_long interface, // IP address to bind to, INADDR_ANY to accept connections
-                      // from any IP
+    u_long interface, // IP address to bind to, INADDR_ANY to accept connections from any IP
     int port,         // port number to bind socket to
     int backlog,      // maximum number of queued connections
-    void (*launch)(void) // pointer to function that will launch the server
+    void (*launch)(struct Server *server) // pointer to function that will launch the server
 ) {
 
   struct Server server; // server struct holding all server info
--- server.h
+++ server.h
@@ -16,9 +16,9 @@ struct Server {
 
   int socket;
 
-  void (*launch)(void);
+  void (*launch)(struct Server *server);
 };
 
-struct Server server_constructor(int domain, int service, int protocol, u_long interface, int port, int backlog, void (*launch)(void));
+struct Server server_constructor(int domain, int service, int protocol, u_long interface, int port, int backlog, void (*launch)(struct Server *server));
 
 #endif /* server_h */
