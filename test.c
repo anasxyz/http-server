1f0e6261bed43fc1f75e69c4137a6a40bd0da794
anasxyz <anaswessam176@gmail.com>
2025-07-12 15:02:19 +0100

    added loop to accept and respond to multiple connections

--- test.c
+++ test.c
@@ -3,30 +3,39 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 void launch(struct Server *server) {
   char buffer[30000];
 
   printf("===== WAITING FOR CONNECTION =====\n");
 
-  int address_length = sizeof(server->address);
-  int new_socket = accept(server->socket, (struct sockaddr *)&server->address, (socklen_t *)&address_length);
+  // infinite loop accepting connections
+  while (1) {
+    int address_length = sizeof(server->address);
+    int new_socket = accept(server->socket, (struct sockaddr *)&server->address, (socklen_t *)&address_length);
 
-  read(new_socket, buffer, 30000);
+    if (new_socket < 0) {
+      perror("Failed to accept connection...\n");
+      exit(1);
+    }
 
-  printf("%s\n", buffer);
+    memset(buffer, 0, sizeof(buffer));
+    read(new_socket, buffer, 30000);
 
-  char *hello =
-    "HTTP/1.1 200 OK\r\n"
-    "Content-Type: text/html\r\n"
-    "Content-Length: 51\r\n"
-    "Connection: close\r\n"
-    "\r\n"
-    "<html><body><h1>Homo Deus</h1></body></html>";
+    printf("Received request: %s\n", buffer);
 
-  write(new_socket, hello, strlen(hello));
+    char *response =
+      "HTTP/1.1 200 OK\r\n"
+      "Content-Type: text/html\r\n"
+      "Content-Length: 51\r\n"
+      "Connection: close\r\n"
+      "\r\n"
+      "<html><body><h1>Hello</h1></body></html>";
 
-  close(new_socket);
+    write(new_socket, response, strlen(response));
+    close(new_socket);
+  }
 }
 
 int main() {
