e2054cff15934a776c466ea9f9547fc0cda2b81f
anasxyz <anaswessam176@gmail.com>
2025-07-15 00:52:12 +0100

    add status code recognition function

--- include/http.h
+++ include/http.h
@@ -8,8 +8,14 @@ typedef struct {
   char *value;
 } Header;
 
+
 typedef struct {
-  char *status;
+  int code;
+  const char* reason;
+} HttpStatus; 
+
+typedef struct {
+  HttpStatus status;
   char *content_type;
   char *body;
   size_t body_length;
@@ -23,7 +29,16 @@ typedef struct {
   char *version;
 } HttpRequest;
 
-void send_response(int socket, HttpResponse *response);
+typedef enum {
+  HTTP_STATUS_OK = 200,
+  HTTP_STATUS_BAD_REQUEST = 400,
+  HTTP_STATUS_NOT_FOUND = 404,
+  HTTP_STATUS_METHOD_NOT_ALLOWED = 405,
+  HTTP_STATUS_INTERNAL_SERVER_ERROR = 500,
+} HttpStatusCode;
+
+void send_response(int socket, HttpStatusCode status_code, const char* text);
+
 void handle_request(int socket, char *request_buffer);
 HttpRequest parse_request(char *request_buffer);
 
