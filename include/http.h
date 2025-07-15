6cece5df90762e1b7755afe17160541ac594eee7
anasxyz <anaswessam176@gmail.com>
2025-07-15 18:50:09 +0100

    separate functions into util files

--- include/http.h
+++ include/http.h
@@ -1,73 +1,13 @@
 #ifndef http_h
 #define http_h
 
+#include "utils_http.h"
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdbool.h>
 
-typedef struct {
-  const char* method;
-  bool allowed;
-} HttpMethod;
-
-static const HttpMethod allowed_methods[] = {
-  {"GET", true},
-  {"POST", false},
-  {"PUT", false},
-  {"DELETE", false},
-  {"PATCH", false},
-  {"HEAD", false},
-  {"OPTIONS", false},
-  {"CONNECT", false},
-  {"TRACE", false},
-};
-
-#define NUM_ALLOWED_METHODS (sizeof(allowed_methods) / sizeof(allowed_methods[0]))
-
-typedef struct {
-  char *key;
-  char *value;
-} Header;
-
-typedef struct {
-  int code;
-  char *reason;
-} HttpStatus;
-
-typedef struct {
-  char *status;
-  char *date;
-  char *server;
-  char *last_modified;
-  char *content_type;
-  char *connection;
-
-  const char *body;
-  size_t body_length;
-  Header *headers;
-  size_t num_headers;
-} HttpResponse;
-
-typedef struct {
-  char *method;
-  char *path;
-  char *version;
-} HttpRequest;
-
-typedef enum {
-  HTTP_STATUS_OK = 200,
-  HTTP_STATUS_BAD_REQUEST = 400,
-  HTTP_STATUS_NOT_FOUND = 404,
-  HTTP_STATUS_METHOD_NOT_ALLOWED = 405,
-  HTTP_STATUS_INTERNAL_SERVER_ERROR = 500,
-} HttpStatusCode;
-
-char *get_status_reason(int code);
-
 HttpResponse *create_response(int status_code, const char *path);
 void send_response(int socket, HttpResponse *response);
-
 void handle_request(int socket, char *request_buffer);
-HttpRequest parse_request(char *request_buffer);
 
 #endif /* http_h */
--- src/http.c
+++ src/http.c
@@ -8,52 +8,8 @@
 
 #include "../include/file_handler.h"
 #include "../include/http.h"
+#include "../include/utils_http.h"
 #include "../include/utils_path.h"
-
-char* get_status_reason(int code) {
-    switch (code) {
-        case 100: return "Continue";
-        case 101: return "Switching Protocols";
-        case 102: return "Processing";
-        case 200: return "OK";
-        case 201: return "Created";
-        case 202: return "Accepted";
-        case 203: return "Non-Authoritative Information";
-        case 204: return "No Content";
-        case 205: return "Reset Content";
-        case 206: return "Partial Content";
-        case 300: return "Multiple Choices";
-        case 301: return "Moved Permanently";
-        case 302: return "Found";
-        case 303: return "See Other";
-        case 304: return "Not Modified";
-        case 307: return "Temporary Redirect";
-        case 308: return "Permanent Redirect";
-        case 400: return "Bad Request";
-        case 401: return "Unauthorized";
-        case 402: return "Payment Required";
-        case 403: return "Forbidden";
-        case 404: return "Not Found";
-        case 405: return "Method Not Allowed";
-        case 406: return "Not Acceptable";
-        case 408: return "Request Timeout";
-        case 409: return "Conflict";
-        case 410: return "Gone";
-        case 411: return "Length Required";
-        case 413: return "Payload Too Large";
-        case 414: return "URI Too Long";
-        case 415: return "Unsupported Media Type";
-        case 426: return "Upgrade Required";
-        case 429: return "Too Many Requests";
-        case 500: return "Internal Server Error";
-        case 501: return "Not Implemented";
-        case 502: return "Bad Gateway";
-        case 503: return "Service Unavailable";
-        case 504: return "Gateway Timeout";
-        case 505: return "HTTP Version Not Supported";
-        default:  return "Unknown Status";
-    }
-}
 
 HttpResponse* create_response(int status_code, const char* path) {
   HttpResponse *response = malloc(sizeof(HttpResponse));
@@ -141,36 +97,6 @@ void send_response(int socket, HttpResponse *response) {
   printf("Content-Type: %s\n", response->content_type);
   printf("Content-Length: %lu\n", response->body_length);
   printf("Connection: %s\n", response->connection);
-}
-
-HttpRequest parse_request(char *request_buffer) {
-  HttpRequest request = {
-      .method = NULL,
-      .path = NULL,
-      .version = NULL,
-  };
-
-  // extract request line
-  char *request_line = strtok(request_buffer, "\r\n");
-  if (request_line) {
-    request.method = strtok(request_line, " ");
-    request.path = strtok(NULL, " ");
-    request.version = strtok(NULL, " ");
-  }
-
-  // if request_line is NULL (extraction failed), HttpRequest fields stay NULL
-  // and then handle_request() can check for errors
-
-  return request;
-}
-
-bool is_method_allowed(const char* method) {
-  for (size_t i = 0; i < NUM_ALLOWED_METHODS; ++i) {
-    if (strcmp(method, allowed_methods[i].method) == 0 && allowed_methods[i].allowed) {
-      return allowed_methods[i].allowed;
-    }
-  }
-  return false;
 }
 
 // handles HTTP request
